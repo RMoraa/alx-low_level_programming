7b9b1f1578c365f04915e69067a11ca815616ae7
RMoraa <moraajillo@gmail.com>
2023-08-15 00:40:49 -0700

    manchester.c

---
+++ simpleshell-prac/manchester.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This Prints Manchester United
+ * Return: This returns 0
+ */
+
+int main(void)
+{
+	printf("Manchester United"); /* Inline Comment*/
+	return (0);
+}
